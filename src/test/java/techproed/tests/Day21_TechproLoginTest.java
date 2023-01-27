71a6a958196e7db983099c4f470367e9e4ffa09c
seymalacin <seymallacinn@gmail.com>
2023-01-27 23:53:03 +0300

    day22

---
+++ src/test/java/techproed/tests/Day21_TechproLoginTest.java
@@ -0,0 +1,33 @@
+package techproed.tests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import techproed.pages.TechproHomePage;
+import techproed.pages.TechproLoginPage;
+import techproed.utilities.ConfigReader;
+import techproed.utilities.Driver;
+
+public class Day21_TechproLoginTest {
+
+
+
+    @Test
+    public void loginTest(){
+        Driver.getDriver().get(ConfigReader.getProperty("techpro_test_url"));
+        TechproLoginPage techproLoginPage = new TechproLoginPage();
+        TechproHomePage techproHomePage = new TechproHomePage();
+        techproLoginPage.username.sendKeys(ConfigReader.getProperty("techpro_test_username"));
+        techproLoginPage.password.sendKeys(ConfigReader.getProperty("techpro_test_password"));
+        techproLoginPage.submitButton.click();
+//      ASSERTION
+//        Login yapildi. Driver su an Home Pagede
+        Assert.assertTrue(techproHomePage.homeHeader.isDisplayed());
+//        Sayfadan cikis yap ve cikis yapildigini test et
+        techproHomePage.homeLogoutButton.click();
+
+//        Cikis olduguna dair assertion yap
+        Assert.assertTrue(techproLoginPage.submitButton.isDisplayed());
+
+ //       Driveri kapat
+        Driver.closeDriver();
+    }
+}
